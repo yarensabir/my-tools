60e719bbbb99e75d0c1fcd547ab948a291dc3f79
yarensabir <yaren.s.ae@gmail.com>
2022-03-29 14:58:45 +0300

    yorum satırı ve açıklamalar gelecek

--- label_selector.py
+++ label_selector.py
@@ -1,9 +1,13 @@
+import shutil
+
 def label_selector(path1, path2, fileCount, classCount, indeks):
     counter = 0
     for i in range(fileCount+1,1,-1):
         try: 
             file =  path1 + str(i) + ".txt" 
             lines = open(file).readlines()
+            img_source = path1 + str(i) + ".jpg"
+            img_copy = path2 + str(i) + ".jpg"
             for line in lines:
                 text, *_ = line.split(" ") 
                 for j in range(0,classCount): 
@@ -13,6 +17,7 @@ def label_selector(path1, path2, fileCount, classCount, indeks):
                             dosya = path2 + str(i) + ".txt"
                             with open(dosya, 'a') as f:
                                 f.write(line)
+                                #shutil.copyfile(img_source, img_copy)  
                             break                  
         except Exception:
             print("exception")
